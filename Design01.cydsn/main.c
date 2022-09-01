219553299eada284267d0a8da72b54101e616ec6
lucasg1021 <lgillet@siue.edu>
2022-08-31 19:51:21 -0500

    temperature and humidity reading working

--- Design01.cydsn/main.c
+++ Design01.cydsn/main.c
@@ -23,9 +23,10 @@ CY_ISR(uart_int_Handler){
 
 void initializeAHT();
 void takeMeasurementAHT();
-void RestartAHT();
-int convert8bto16b(uint8 num1, uint8 num2);
-float convertTemp(uint8 num1, uint8 num2, uint8 num3);
+void restartAHT();
+
+float convertTempF(uint8 num1, uint8 num2, uint8 num3);
+float convertHumidity(uint8 num1, uint8 num2, uint8 num3);
 
 
 int main(void)
@@ -41,85 +42,46 @@ int main(void)
     SCL_Write(0);
     CyDelay(10);
 
-    RestartAHT();
-    
-    SCL_Write(1);
-    CyDelay(20);
+    restartAHT();
     
     char s[80];
     uint8 i2cWrBuf[3];
-    uint8 i2cRdBuf[10];
-    uint8 num1, num2, num3;
+    uint8 i2cRdBuf[7];
     
-    float tempC;
+    float tempF, humidity;
     
     CyDelay(40); // wait 40ms after AHT power on
     initializeAHT();
     CyDelay(80); // wait 80ms for measurement to complete
-       
-    sprintf(s, "initialized\r\n");
-    UART_PutString(s); 
-    
-    i2cWrBuf[0] = 0b01110001; // slave addr ; b[0] = 1 for read mode
-    I2C_MasterWriteBuf(AHT_ADDR, (uint8 *)i2cWrBuf, 1, I2C_MODE_COMPLETE_XFER);
-    while(SDA_Read());
-    while(!(I2C_MasterStatus() & I2C_MSTAT_WR_CMPLT));
-    I2C_MasterClearStatus();
-    CyDelay(10);
-   
-
-    I2C_MasterReadBuf(AHT_ADDR, (uint8 *)i2cRdBuf, 7, I2C_MODE_COMPLETE_XFER); // get status
-    while (!I2C_MSTAT_RD_CMPLT);
-    I2C_MasterClearStatus();
-    CyDelay(10);
-    
-    
-    while(i2cRdBuf[0] & (1 << 7)); //check that bit 7 (busy) is low
-    
-     //print status a measurement values
-    for(int i = 0; i < 7; i++){
-        sprintf(s, "%i\r\n", i2cRdBuf[i]);
-        UART_PutString(s);
-    }
-    
-    num1 = i2cRdBuf[5];
-    num2 = i2cRdBuf[4];
-    num3 = i2cRdBuf[3];
-    tempC = convertTemp(num1, num2, num3);
-    sprintf(s, "%.2f\r\n", tempC);
-    UART_PutString(s);
-    
-    I2C_MasterClearReadBuf();
     
     for(;;)
     {
         takeMeasurementAHT();
         
-            i2cWrBuf[0] = 0b01110001; // slave addr ; b[0] = 1 for read mode
-    I2C_MasterWriteBuf(AHT_ADDR, (uint8 *)i2cWrBuf, 1, I2C_MODE_COMPLETE_XFER);
-    while(SDA_Read());
-    while(!(I2C_MasterStatus() & I2C_MSTAT_WR_CMPLT));
-    I2C_MasterClearStatus();
-    CyDelay(10);
-   
+        i2cWrBuf[0] = 0b01110001; // slave addr ; b[0] = 1 for read mode
+        I2C_MasterWriteBuf(AHT_ADDR, (uint8 *)i2cWrBuf, 1, I2C_MODE_COMPLETE_XFER);
+        while(SDA_Read());
+        while(!(I2C_MasterStatus() & I2C_MSTAT_WR_CMPLT));
+        I2C_MasterClearStatus();
+        CyDelay(10);
+       
 
-    I2C_MasterReadBuf(AHT_ADDR, (uint8 *)i2cRdBuf, 7, I2C_MODE_COMPLETE_XFER); // get status
-    while (!I2C_MSTAT_RD_CMPLT);
-    I2C_MasterClearStatus();
-    CyDelay(10);
+        I2C_MasterReadBuf(AHT_ADDR, (uint8 *)i2cRdBuf, 7, I2C_MODE_COMPLETE_XFER); // get status
+        while (!I2C_MSTAT_RD_CMPLT);
+        I2C_MasterClearStatus();
+        CyDelay(10);
     
-    while(i2cRdBuf[0] & (1 << 7)); //check that bit 7 (busy) is low
-    
-    num1 = i2cRdBuf[5];
-    num2 = i2cRdBuf[4];
-    num3 = i2cRdBuf[3];
-    tempC = convertTemp(num1, num2, num3);
-    sprintf(s, "%.2f\r\n", tempC);
-    UART_PutString(s);
-    
-    I2C_MasterClearReadBuf();
+        while(i2cRdBuf[0] & (1 << 7)); //check that bit 7 (busy) is low
+        
+        humidity = convertHumidity(i2cRdBuf[1], i2cRdBuf[2], i2cRdBuf[3]);
+        tempF = convertTempF(i2cRdBuf[5], i2cRdBuf[4], i2cRdBuf[3]);
+        sprintf(s, "Temperature: %.2f F\r\nHumidity: %.2f \%% \r\n", tempF, humidity);
+        UART_PutString(s);
+        
+        I2C_MasterClearReadBuf();
+        memset(i2cRdBuf, 0, sizeof i2cRdBuf);
 
-    CyDelay(500);
+        CyDelay(1000);
     }
 }
 
@@ -171,36 +133,43 @@ void takeMeasurementAHT(){
     CyDelay(80);
 }
 
-void RestartAHT(){
-    uint8 i2cWrBuf[1];
-
-    i2cWrBuf[0] = 0b01110000; // slave addr ; b[0] = 0 for write mode
-    I2C_MasterWriteBuf(AHT_ADDR, i2cWrBuf, 1, I2C_MODE_COMPLETE_XFER);
-    while(SDA_Read()); // SDA pulled low for ACK
-    I2C_MasterClearStatus();
+void restartAHT(){
+    uint8 i2cWrBuf[2];
     
-    i2cWrBuf[0] = 0b10111010;
-    I2C_MasterWriteBuf(AHT_ADDR, i2cWrBuf, 1, I2C_MODE_COMPLETE_XFER);
-    while(SDA_Read()); // SDA pulled low for ACK
+    i2cWrBuf[0] = 0b01110000; // slave addr ; b[0] = 1 for read mode
+    i2cWrBuf[1] = 0b10111010;
+    I2C_MasterWriteBuf(AHT_ADDR, (uint8 *)i2cWrBuf, 2, I2C_MODE_COMPLETE_XFER);
+    while(SDA_Read());
+    while(!(I2C_MasterStatus() & I2C_MSTAT_WR_CMPLT));
     I2C_MasterClearStatus();
+    CyDelay(10);
 }
 
-int convert8bto16b(uint8 num1, uint8 num2){
-    int result = (num2 << 8) | num1;
-    
-    return result;
-}
-
-float convertTemp(uint8 num1, uint8 num2, uint8 num3){
+float convertTempF(uint8 num1, uint8 num2, uint8 num3){
     // combine 2 8 bit temperature data bytes and lower 4 bits of humidity/temperature byte
     int num16b = (num2 << 8) | num1;   
     int lowernum3 = num3 & 0x0F;
     int num20b = (lowernum3 << 16) | num16b;
     
-    //convert to temp in C using 
-    float result = (((double)num20b * 200.0)/pow(2.0, 20.0)) - 50.0;
+    //convert to temp in C using formula in datasheet
+    float tempC = (((double)num20b * 200.0)/pow(2.0, 20.0)) - 50.0;
+    
+    //convert to F
+    float tempF = (tempC * 9/5) + 32.0;
+    
+    return tempF;
+}
+
+
+float convertHumidity(uint8 num1, uint8 num2, uint8 num3){
+    // combine 2 8 bit humidity data bytes and upper 4 bits of humidity/temperature byte
+    int num16b = (num1 << 8) | num2;
+    int uppernum3 = num3 >> 4;
+    int num20b = (num16b << 4) | uppernum3;
+    
+    //convert to humidity percentage using formula in datasheet
+    float result = ((double)num20b/pow(2.0, 20.0)) * 100.0;
     
     return result;
 }
-
 /* [] END OF FILE */
