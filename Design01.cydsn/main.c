16fbe23a0ed85368e58626e6de2e4b884325f63c
lucasg1021 <lgillet@siue.edu>
2022-09-06 22:30:37 -0500

    added printing temp and humidity to OLED display

--- Design01.cydsn/main.c
+++ Design01.cydsn/main.c
@@ -10,15 +10,28 @@
  * ========================================
 */
 #include "project.h"
+#include "ssd1306.h"
+
 #include <stdio.h>
 #include <math.h>
 
 #define AHT_ADDR (uint8)(0x38)
+#define DISPLAY_ADDRESS (0x3C)
 
 CY_ISR(uart_int_Handler){
     char c = UART_GetChar();
     UART_PutChar(c);
+    ESPUART_PutChar(c);
     UART_ClearRxBuffer();
+}
+
+CY_ISR(esp_int_Handler){
+//    char c;
+//    while(ESPUART_GetRxBufferSize() > 0){
+//        c = ESPUART_GetChar();
+//        UART_PutChar(c);
+//    }
+//    ESPUART_ClearRxBuffer();
 }
 
 void initializeAHT();
@@ -32,32 +45,41 @@ float convertHumidity(uint8 num1, uint8 num2, uint8 num3);
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
-
+    
+    ESPUART_Start();
     UART_Start();
     rx_int_StartEx(uart_int_Handler);
+    esprx_int_StartEx(esp_int_Handler);
     
     I2C_Start();
     
-    // start sensor
-    SCL_Write(0);
-    CyDelay(10);
-
-    restartAHT();
     
     char s[80];
     uint8 i2cWrBuf[3];
     uint8 i2cRdBuf[7];
-    
+     
     float tempF, humidity;
     
-    CyDelay(40); // wait 40ms after AHT power on
+//    while(1){
+//        sprintf(s, "AT\r\n");
+//        ESPUART_PutString(s);
+//        CyDelay(1000);
+//    }
+
+    CyDelay(100); // wait 40ms after AHT power on
     initializeAHT();
     CyDelay(80); // wait 80ms for measurement to complete
     
+    // initialize OLED
+    // must stop sending and clear status to work with AHT
+    display_init(DISPLAY_ADDRESS);
+    I2C_MasterSendStop();
+    I2C_MasterClearStatus();
+    
     for(;;)
-    {
+    {        
         takeMeasurementAHT();
-        
+
         i2cWrBuf[0] = 0b01110001; // slave addr ; b[0] = 1 for read mode
         I2C_MasterWriteBuf(AHT_ADDR, (uint8 *)i2cWrBuf, 1, I2C_MODE_COMPLETE_XFER);
         while(SDA_Read());
@@ -65,19 +87,39 @@ int main(void)
         I2C_MasterClearStatus();
         CyDelay(10);
        
-
         I2C_MasterReadBuf(AHT_ADDR, (uint8 *)i2cRdBuf, 7, I2C_MODE_COMPLETE_XFER); // get status
         while (!I2C_MSTAT_RD_CMPLT);
         I2C_MasterClearStatus();
         CyDelay(10);
-    
+        
         while(i2cRdBuf[0] & (1 << 7)); //check that bit 7 (busy) is low
         
+        // convert readings to temp and humidity
         humidity = convertHumidity(i2cRdBuf[1], i2cRdBuf[2], i2cRdBuf[3]);
         tempF = convertTempF(i2cRdBuf[5], i2cRdBuf[4], i2cRdBuf[3]);
-        sprintf(s, "Temperature: %.2f F\r\nHumidity: %.2f \%% \r\n", tempF, humidity);
+        
+        //print temp to uart
+        sprintf(s, "Temperature: %.2f F\r\n", tempF);
         UART_PutString(s);
         
+        //clear and set up OLED settings, print temp
+        display_clear();    
+        display_update();    
+        gfx_setTextSize(1);
+        gfx_setTextColor(WHITE);
+        gfx_setCursor(2, 2);
+        gfx_println(s);
+        
+        //print humidity to uart and OLED
+        sprintf(s, "Humidity: %.2f \%% \r\n", humidity);
+        UART_PutString(s);
+        gfx_setCursor(2, 20);
+        gfx_println(s);
+        display_update(); 
+        I2C_MasterSendStop();
+        I2C_MasterClearStatus();
+        
+        //clear I2C buffer
         I2C_MasterClearReadBuf();
         memset(i2cRdBuf, 0, sizeof i2cRdBuf);
 
@@ -94,7 +136,7 @@ void initializeAHT(){
     while(!(I2C_MasterStatus() & I2C_MSTAT_WR_CMPLT));
     I2C_MasterClearStatus();
     CyDelay(10);
-    
+        
     I2C_MasterReadBuf(AHT_ADDR, i2cRdBuf, 1, I2C_MODE_COMPLETE_XFER);
     while(!I2C_MSTAT_RD_CMPLT);
     I2C_MasterClearStatus();
@@ -136,10 +178,9 @@ void takeMeasurementAHT(){
 void restartAHT(){
     uint8 i2cWrBuf[2];
     
-    i2cWrBuf[0] = 0b01110000; // slave addr ; b[0] = 1 for read mode
+    i2cWrBuf[0] = 0b01110000; // slave addr ; b[0] = 0 for write mode
     i2cWrBuf[1] = 0b10111010;
     I2C_MasterWriteBuf(AHT_ADDR, (uint8 *)i2cWrBuf, 2, I2C_MODE_COMPLETE_XFER);
-    while(SDA_Read());
     while(!(I2C_MasterStatus() & I2C_MSTAT_WR_CMPLT));
     I2C_MasterClearStatus();
     CyDelay(10);
